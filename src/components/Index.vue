1b504ce6eddd33104c34c2998680983a24aa3e9b
hanitf <91092572+hanitf@users.noreply.github.com>
2023-09-23 07:51:53 +0200

    new visual

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -9,7 +9,6 @@
         <img class="indexlogo" src="./logos/png/logowhite.png" alt="Logo">
       </div>
       <div class="indexname">
-        <p>Web Developer</p>
         <h1>Felipe Hanitzsch</h1>
         <p>Welcome to my portfolio, feel free to take a look on my <a href="https://github.com/hanitf">Github</a> page and
           my <a href="https://www.linkedin.com/in/felipe-hanitzsch-33b625122/">
@@ -18,27 +17,28 @@
     </div>
     <div class="aboutme" id="aboutme">
       <h1>About me</h1>
+      <img src="./logos/680-it-developer.gif" alt="About me logo" class="aboutmelogo">
       <p>I have been passionate about technology since I was a child, I had experience with IT support in video production
         companies, where I had my first contact with software development, covering the need to maintain the rendering of
         images on farm servers, I currently work in the logistics area, and I'm looking for my first experience as a web
         developer (Front-end), I've completed several courses in the area, highlighting the Ironhack bootcamp, where we
-        worked with the main web development technologies, HTML5, CSS3 and JavaScript, always connected by Vue.js, the
-        course guided us through challenges with API's, Data bases like Supabase, and development of interactive and
-        complex web applications. Don't hesitate to visit my Portfolio or my GitHub.</p>
+        worked with the main web development technologies.</p>
     </div>
     <div class="skills" id="skills">
       <h1>Skills</h1>
       <img src="./logos/skillslogo.png" alt="Skills logo" class="skillslogo">
       <p>Web developer in Html, CSS, Javascript and Vue.js</p>
-      <p>Welcome to my portfolio, feel free to take a look on my <a href="https://github.com/hanitf">Github</a> page and
-        my <a href="https://www.linkedin.com/in/felipe-hanitzsch-33b625122/">
-          Linkedin</a>, do not resitate to contact me!</p>
+      <p>Ironhack certified in HTML5, CSS3 and JavaScript, always connected by Vue.js, the
+        course guided us through challenges with API's, Data bases like Supabase, and development of interactive and
+        complex web applications.</p>
     </div>
+    
     <div class="projects" id="projects">
-      <h1>Projects</h1>
-      <iframe src="https://itodohanitzsch.netlify.app/" frameborder="0" height="300" width="300"></iframe>
-      <iframe src="https://itodohanitzsch.netlify.app/" frameborder="0" height="300" width="300"></iframe>
-      <iframe src="https://itodohanitzsch.netlify.app/" frameborder="0" height="300" width="300"></iframe>
+      <h1 class="projectname">Projects</h1>
+      <div class="projectframe">
+      <iframe src="https://itodohanitzsch.netlify.app/" frameborder="0" height="300" width="500"></iframe>
+      <iframe src="https://itodohanitzsch.netlify.app/" frameborder="0" height="300" width="500"></iframe>
+    </div>
     </div>
   </div>
 </template>
@@ -52,12 +52,15 @@
 .indexcss {
   display: flex;
   flex-direction: row;
-  padding-top: 150px;
-  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  padding-top: 110px;
+  font-family: 'Segoe UI', Tahoma, Verdana, sans-serif;
+  align-items: center;
+  height: 900px;
 }
 
 .indexname {
   padding-left: 50px;
+  font-size: 23px;
 }
 
 .indexname,
@@ -67,8 +70,8 @@ a {
 }
 
 .indexlogo {
-  width: 170px;
-  height: 150px;
+  width: 370px;
+  height: 350px;
   border: 3px solid #ccc;
   border-radius: 50%;
   background: #eee;
@@ -80,5 +83,43 @@ a {
   width: 190px;
   height: 170px;
 }
+
+.aboutmelogo{
+  width: 390px;
+  height: 370px;
+}
+
+.aboutme{
+  display: flex;
+  height: 900px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 25px;
+  word-spacing: 2px;
+}
+.skills{
+  display: flex;
+  height: 900px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 25px;
+  word-spacing: 2px;
+}
+
+.projects{ 
+  display: flex;
+  height: 900px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.projectframe{
+display: flex;
+flex-direction: row;
+}
+
 
 </style>
